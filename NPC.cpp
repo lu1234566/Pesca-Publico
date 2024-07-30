2508840fc20cba74a2b0331bd6a541c27b744e67
lu1234566 <barcelar34@gmail.com>
2024-07-30 20:16:54 -0300

    Update NPC.cpp

--- NPC.cpp
+++ NPC.cpp
@@ -1,23 +1,22 @@
 #include "NPC.hpp"
 
-NPC::NPC(const std::string &name, const sf::Vector2f &position, const std::string &dialogue) 
-    : name(name), dialogue(dialogue) {
-    // Carregar a textura e definir a posição inicial
-    if (!texture.loadFromFile("npc.png")) {
-        // Handle error
+NPC::NPC(const std::string &name, const sf::Vector2f &position, const std::string &dialogue)
+    : name(name), position(position), dialogue(dialogue) {
+    if (!texture.loadFromFile("npc.gif")) {
+        std::cerr << "Erro ao carregar a textura do NPC" << std::endl;
     }
     sprite.setTexture(texture);
     sprite.setPosition(position);
 }
 
-void NPC::draw(sf::RenderWindow &window) {
+void NPC::draw(sf::RenderWindow &window) const {
     window.draw(sprite);
 }
 
-const sf::FloatRect NPC::getBounds() const {
+sf::FloatRect NPC::getBounds() const {
     return sprite.getGlobalBounds();
 }
 
-const std::string& NPC::getDialogue() const {
+std::string NPC::getDialogue() const {
     return dialogue;
 }
