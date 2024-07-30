7065605a0a688d1166b789de68afb25fb196fa9b
lu1234566 <barcelar34@gmail.com>
2024-07-30 20:04:03 -0300

    Update NPC.hpp

--- NPC.hpp
+++ NPC.hpp
@@ -2,15 +2,17 @@
 #define NPC_HPP
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include <string>
 
 class NPC {
 public:
-    NPC(const std::string &name, const sf::Vector2f &position, const std::string &dialogue);
-    
-    void draw(sf::RenderWindow &window);
-    const sf::FloatRect getBounds() const;
-    const std::string& getDialogue() const;
+    std::string name;
+    std::vector<std::string> dialogues;
+    sf::Sprite sprite;
+
+    void draw(sf::RenderWindow &window) const; // Adicionando const
+};
 
 private:
     std::string name;
